955de25013f1c93f7334669bdc0c6208bd8a75bf
VKormylo <vitaliykormylo@gmail.com>
2022-05-29 18:42:54 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3d1c2c99-9236-4d19-88a3-8450fde5df81';
+export const MODAL_CLOSE_SECONDS = 2.5;
